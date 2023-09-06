e6949e66b476b9af8328a94a21ed5d3aa75b5c31
Eduardo Scheunemann <dudu.jefe@hotmail.com>
2023-09-06 11:43:52 -0300

    commit de hojee

---
+++ build/aula05.js
@@ -0,0 +1,9 @@
+"use strict";
+let nomes = ['Eduardo', 'Henrique', 'Francisco'];
+let macaco;
+macaco = 'banana';
+macaco = 1;
+macaco = true;
+nomes.push('manoel');
+nomes.sort();
+console.log(macaco);
