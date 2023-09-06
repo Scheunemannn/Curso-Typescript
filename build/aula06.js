a119b29e7de602a06a1c290cd7afefe0ced94853
Eduardo Scheunemann <dudu.jefe@hotmail.com>
2023-09-06 13:21:53 -0300

    aula de tuplas

--- build/aula06.js
+++ build/aula06.js
@@ -1,1 +1,9 @@
 "use strict";
+let numeros = [3, 4, 7, 4, 1, 6];
+numeros.push(2);
+numeros.unshift(5);
+numeros.sort();
+numeros.pop();
+numeros.shift();
+console.log(numeros);
+let numerosRo = [100, 300, 500];
---
+++ src/aula06.ts
@@ -0,0 +1,15 @@
+let numeros:Array<number> = [3,4,7,4,1,6]
+//adiciona um novo elemento a posição final do array
+numeros.push(2)
+//adiciona um elemento a posicao inicial do array
+numeros.unshift(5)
+//organiza o array em ordem crescente, do maior para o menor
+numeros.sort()
+//retira um elemento da posicao final do array
+numeros.pop()
+//retira um elemento da posicao inicial do array
+numeros.shift()
+
+console.log(numeros)
+//um tipo de array que serve somente para leitura, nao pode ser modificado por funcoes ou metodos de insercao e remocao
+let numerosRo:ReadonlyArray<number> = [100, 300, 500]
